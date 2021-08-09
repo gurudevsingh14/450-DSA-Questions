3be2f31b0b207710aa421735c7d1b07af7785e61
Gurudev singh <gurudev620.gs@gmail.com>
2021-08-09 15:23:16 +0530

    Middle of the Linked List

---
+++ LinkedList/Middle of the Linked List.cpp
@@ -0,0 +1,11 @@
+//problem link: https://leetcode.com/problems/middle-of-the-linked-list/
+
+ListNode* middleNode(ListNode* head) {
+    ListNode* fast = head, *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
